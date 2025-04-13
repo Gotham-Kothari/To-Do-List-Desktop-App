359352ce75078b61e1dd22c885fc1c485dd731cb
Gotham_Kothari <gothamkothari640@gmail.com>
2025-04-13 12:25:15 +0530

    Added delete button functionality to task list items

--- renderer.js
+++ renderer.js
@@ -18,11 +18,27 @@ function addTask() {
         const span = document.createElement("span");
         span.className = "task-text";
         span.textContent = taskText;
+        
+        //Delete button
+        const deletebtn = document.createElement("span");
+        deletebtn.innerHTML = "&times;";
+        deletebtn.classList.add("delete-btn");
+        deletebtn.onclick = () => li.remove();
+        deletebtn.style.marginLeft = "auto";
+        deletebtn.style.marginRight = "10px";
+        deletebtn.style.cursor = "pointer";
 
         //Adding the task into the list of tasks
         li.appendChild(checkbox);
         li.appendChild(span);
+        li.appendChild(deletebtn);
         taskList.appendChild(li);
+
+        // checkbox.addEventListener("change", function() {
+        //     if(checkbox.checked) {
+        //         li.remove();
+        //     }
+        // });
 
         taskInput.value = ""; //Clearing Input
     }
